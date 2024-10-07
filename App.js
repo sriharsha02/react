a820cc72ace6ba4e4a5999cefb7d7e653eed4575
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-10-07 16:19:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,3 +7,91 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
 console.log(heading);
+
+//Exercise problem
+
+/*
+
+div id = parent
+  div id = child
+      h1
+  div
+div
+
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h2", {}, "Hello from h2 tag")
+  )
+);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+
+root2.render(parent);
+console.log(parent);
+
+/*
+
+div id = parent
+  div id = child
+      h1
+      h2
+  div
+div
+
+*/
+
+const parent2 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello from h1 tag"),
+    React.createElement("h2", {}, "I am from h2 tag"),
+  ])
+);
+
+const root3 = ReactDOM.createRoot(document.getElementById("root"));
+
+root2.render(parent2);
+console.log(parent2);
+
+/*
+div id = parent
+  div id = child
+      h1
+      h2
+  div
+div
+div id = parent
+  div id = child
+      h1
+      h2
+  div
+div
+
+
+
+
+*/
+
+const parent3 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello from h1 tag"),
+    React.createElement("h2", {}, "I am from h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello from h1 tag"),
+    React.createElement("h2", {}, "I am from h2 tag"),
+  ]),
+]);
+
+const root4 = ReactDOM.createRoot(document.getElementById("root"));
+
+root2.render(parent2);
+console.log(parent2);
