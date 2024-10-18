34ae11c2a9631ac91853d8444e4cdf37c49b0c53
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-10-18 16:47:24 +0530

    creating functional components

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element --> Object --> HTML Element (rendering to DOM)
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
 //JSX
 
-const jsxHeading = <h1>Namaste React using JSX</h1>;
+const heading = <h1>Namaste React using JSX</h1>;
+console.log(heading);
 
-console.log(jsxHeading);
-root.render(jsxHeading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+//React Component
+//class based component --> old way
+//functional component --> new way
+
+//React Functional Component --> A normal JS function
+
+const TitleComponent = () => {
+  return <h1>Title from Namaste React</h1>;
+};
+// const HeadingComponent = () => {
+//   <TitleComponent />;
+//   return <h1>Namaste React Functional Component</h1>;
+// };
+
+const number = 1000;
+//Another way of writing without return
+const HeadingComponent = () => (
+  <div id="container">
+    {heading}
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+    {TitleComponent()}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
+root.render(<HeadingComponent />);
