ec271fd6385290e6a06d4eafa5877c8d89da98be
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2025-01-01 20:21:10 +0530

    filtering based on rating

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,18 @@ import resList from "../utils/mockData";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log();
+            resList = resList.filter((res) => res.info.avgRating > 4);
+            console.log(resList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
